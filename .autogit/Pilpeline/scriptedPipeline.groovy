d560f0917cf7955f8d5aa42c60362d41391fb788
Hitesh Mankar <Hiteshmankar777@gmail.com>
2022-06-07 11:03:36 +0530

    --- Auto Git Commit ---

---
+++ .autogit/Pilpeline/scriptedPipeline.groovy
@@ -0,0 +1,14 @@
+node {  
+    stage('Pull') { 
+        git 'https://github.com/shubhamkalsait/studentapp-ui.git'
+    }
+    stage('Build') { 
+        echo 'Buidl Successful'
+    }
+    stage('Test') { 
+        echo 'test successful' 
+    }
+    stage('Deploy') { 
+        echo 'deploy successful'
+    }
+}
