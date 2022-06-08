a88416f24246707ad6e4d4a266d4be1d37f1b2da
Hitesh Mankar <Hiteshmankar777@gmail.com>
2022-06-08 08:11:27 +0530

    --- Auto Git Commit ---

--- .autogit/Pilpeline/seedjob/seedjob2.groovy
+++ .autogit/Pilpeline/seedjob/seedjob2.groovy
@@ -3,10 +3,8 @@ pipelineJob('scripted-2') {
   description('')
 
   displayName('scripted-2'
-
   configure { flowdefinition ->
-
-    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps@2725.v7b_c717eb_12ce') {
+   flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps@2725.v7b_c717eb_12ce') {
 
       'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git@4.11.3') {
 
