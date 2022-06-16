a640ce0c2c25439f13657e8993b9eb2a7f91e2c8
Hitesh Mankar <Hiteshmankar777@gmail.com>
2022-06-16 16:07:45 +0530

    --- Auto Git Commit ---

--- .autogit/Pilpeline/seedjob/seedjob3.groovy
+++ .autogit/Pilpeline/seedjob/seedjob3.groovy
@@ -6,14 +6,6 @@ pipelineJob('scripted-pipeline_converted') {
 
   keepDependencies(false)
 
-  quietPeriod(0)
-
-  checkoutRetryCount(0)
-
-  disabled(false)
-
-  concurrentBuild(false)
-
   configure { flowdefinition ->
 
     flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps@2725.v7b_c717eb_12ce') {
